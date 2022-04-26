9c252b39c28385772424feeae1708933dc761b55
Jonathan Gonzaga <jonathan.s.gonzaga@gmail.com>
2022-04-25 21:16:03 -0300

    Create project
    Adding files

---
+++ src/lvgl_sample.h
@@ -0,0 +1,6 @@
+#ifndef LVGL_SAMPLE_H
+#define LVGL_SAMPLE_H
+
+void lv_sample(void);
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,31 @@
+#include <device.h>
+#include <drivers/display.h>
+#include <lvgl.h>
+#include <stdio.h>
+#include <string.h>
+#include <zephyr.h>
+#include "lvgl_sample.h"
+
+#define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
+#include <logging/log.h>
+LOG_MODULE_REGISTER(app);
+
+void main(void)
+{
+	const struct device *display_dev;
+
+	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+	if (!device_is_ready(display_dev)) {
+		LOG_ERR("Device not ready, aborting test");
+		return;
+	}
+
+	lv_sample();
+	lv_task_handler();
+	display_blanking_off(display_dev);
+
+	while (1) {
+		lv_task_handler();
+		k_sleep(K_MSEC(10));
+	}
+}
